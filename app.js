720a4656917ba47b92726e52fa1ed009d9f9658c
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-02-23 07:48:01 +0530

    postman api call method post

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require("express")
 
 const app = express()
 
-app.listen(5000)
+app.use(express.json())
+
+app.listen(5000,()=>{
+    console.log("Server is running")
+})
+
+const notes = []
+
+app.post("/notes", (req, res)=>{
+
+    console.log(req.body)
+    res.send("notes created")
+})
